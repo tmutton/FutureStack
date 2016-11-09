79c10f69a34c8e4465bbefc8b912d802650711f4
iancooper <ian_hammond_cooper@yahoo.co.uk>
2016-11-09 12:23:32 +0000

    get to a single get against root

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureStack.Controllers
+{
+    [Route("todo-backend")]
+    public class ToDoController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"value1", "value2"};
+        }
+   }
+}
+
+

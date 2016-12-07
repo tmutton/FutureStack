679d2c459e3fccd919e40c0a0b4f4ae20facc04f
toby.henderson <toby.henderson@huddle.com>
2016-12-07 18:49:51 +0000

    Adds SimpleInjector IOC to API and uses sqlite DB

--- todo-backend/Startup.cs
+++ todo-backend/Startup.cs
@@ -1,13 +1,26 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using paramore.brighter.commandprocessor;
+using Polly;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using ToDoCore.Adaptors.Db;
+using ToDoCore.Ports.Commands;
+using ToDoCore.Ports.Handlers;
 
 namespace FutureStack
 {
     public class Startup
     {
+        private Container container = new Container();
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -33,6 +46,9 @@ namespace FutureStack
             // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
 
+            services.AddDbContext<ToDoContext>(options =>
+                    options.UseSqlite("Data Source=ToDoDb.sqlite"));
+
             services.AddMvc();
 
             services.AddCors(options =>
@@ -41,13 +57,23 @@ namespace FutureStack
                     builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
                 );
             });
+
+            services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(new SimpleInjectorViewComponentActivator(container));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseSimpleInjectorAspNetRequestScoping(container);
+            container.Options.DefaultScopedLifestyle = new AspNetRequestLifestyle();
+            InitializeContainer(app);
+            container.Verify();
+
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
 
             app.UseCors("AllowAll");
 
@@ -56,6 +82,67 @@ namespace FutureStack
             app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseMvc();
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            RegisterCommandProcessor();
+
+            // Cross-wire ASP.NET services (if any). For instance:
+            container.RegisterSingleton(app.ApplicationServices.GetService<ILoggerFactory>());
+            container.RegisterSingleton(app.ApplicationServices.GetService<DbContextOptions<ToDoContext>>());
+            // NOTE: Prevent cross-wired instances as much as possible.
+            // See: https://simpleinjector.org/blog/2016/07/
+        }
+
+
+        private void RegisterCommandProcessor()
+        {
+            //create handler 
+            var subscriberRegistry = new SubscriberRegistry();
+            container.Register<IHandleRequests<AddToDoCommand>, AddToDoCommandHandler>(Lifestyle.Scoped);
+            subscriberRegistry.Register<AddToDoCommand, AddToDoCommandHandler>();
+
+            //create policies
+            var retryPolicy = Policy.Handle<Exception>().WaitAndRetry(new[] { TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(100), TimeSpan.FromMilliseconds(150) });
+            var circuitBreakerPolicy = Policy.Handle<Exception>().CircuitBreaker(1, TimeSpan.FromMilliseconds(500));
+            var policyRegistry = new PolicyRegistry() { { CommandProcessor.RETRYPOLICY, retryPolicy }, { CommandProcessor.CIRCUITBREAKER, circuitBreakerPolicy } };
+
+            var servicesHandlerFactory = new ServicesHandlerFactory(container);
+
+            var commandProcessor = CommandProcessorBuilder.With()
+                .Handlers(new HandlerConfiguration(subscriberRegistry, servicesHandlerFactory))
+                .Policies(policyRegistry)
+                .NoTaskQueues()
+                .RequestContextFactory(new InMemoryRequestContextFactory())
+                .Build();
+
+            container.RegisterSingleton<IAmACommandProcessor>(commandProcessor);
+        }
+
+        private class ServicesHandlerFactory : IAmAHandlerFactory
+        {
+            private readonly Container _serviceProvider;
+
+            public ServicesHandlerFactory(Container serviceProvider)
+            {
+                _serviceProvider = serviceProvider;
+
+            }
+            public IHandleRequests Create(Type handlerType)
+            {
+                return _serviceProvider.GetInstance(handlerType) as IHandleRequests;
+            }
+
+            public void Release(IHandleRequests handler)
+            {
+                var disposable = handler as IDisposable;
+                disposable?.Dispose();
+            }
         }
     }
 }

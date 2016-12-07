cf0b08a3538ea6cf2b4680e4e62c2177a85cc2a8
ian <ian@huddle.com>
2016-12-07 10:00:27 +0000

    Change the project structure so that the website is in a sub-folder, not at teh root

--- ToDo-Core/Ports/Commands/AddToDoCommand.cs
+++ ToDo-Core/Ports/Commands/AddToDoCommand.cs
@@ -1,10 +1,13 @@
 ﻿
+using System;
 using  paramore.brighter.commandprocessor;
 
 namespace ToDoCore.Ports.Commands
 {
     public class AddToDoCommand : Command
     {
-
+        public AddToDoCommand() : base(Guid.NewGuid())
+        {
+        }
     }
 }

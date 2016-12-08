f35517f90eeb6d826ebe5a139edd7003412e3fde
ian <ian@huddle.com>
2016-12-08 19:00:04 +0000

    Building

---
+++ ToDo-Test/Core/Adaptors/ViewModelRetrievers/ToDoViewModelRetrieverTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace ToDoTest.Core.Adaptors.ViewModelRetrievers
+{
+    [TestFixture]
+    public class ToDoViewModelRetrieverTests
+    {
+        public void Test
+    }
+}

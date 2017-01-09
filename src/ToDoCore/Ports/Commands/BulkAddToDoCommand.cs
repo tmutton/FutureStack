d46893a5a175b528ae5cc9fa53ea123aa810019d
ian <ian@huddle.com>
2017-01-09 10:43:09 +0000

    add a bulk todo endpoint handler

---
+++ src/ToDoCore/Ports/Commands/BulkAddToDoCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using paramore.brighter.commandprocessor;
+using ToDoCore.ViewModels;
+
+namespace ToDoCore.Ports.Commands
+{
+    public class BulkAddToDoCommand : Command
+    {
+        public IEnumerable<AddToDoRequest> ToDos { get; }
+        public IList<int> ToDoItemIds { get; } //out parameter
+
+        public BulkAddToDoCommand(IEnumerable<AddToDoRequest> todos) : base(Guid.NewGuid())
+        {
+            ToDos = todos;
+            ToDoItemIds = new List<int>();
+        }
+    }
+}
---
+++ src/ToDoCore/Ports/Handlers/BulkAddToDoCommandHandlerAsync.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using paramore.brighter.commandprocessor;
+using ToDoCore.Adaptors.Db;
+using ToDoCore.Adaptors.Repositories;
+using ToDoCore.Model;
+using ToDoCore.Ports.Commands;
+
+namespace ToDoCore.Ports.Handlers
+{
+    public class BulkAddToDoCommandHandlerAsync: RequestHandlerAsync<BulkAddToDoCommand>
+    {
+        private readonly DbContextOptions<ToDoContext> _options;
+
+        public BulkAddToDoCommandHandlerAsync(DbContextOptions<ToDoContext> options)
+        {
+            _options = options;
+        }
+
+        public override async Task<BulkAddToDoCommand> HandleAsync(BulkAddToDoCommand command,
+            CancellationToken? ct = null)
+        {
+            using (var uow = new ToDoContext(_options))
+            {
+                var repository = new ToDoItemRepositoryAsync(uow);
+                foreach (var todo in command.ToDos)
+                {
+                    var savedItem = await repository.AddAsync(
+                        new ToDoItem {Title = todo.Title, Completed = todo.Completed, Order = todo.Order},
+                        ct ?? default(CancellationToken)
+                    );
+
+                    command.ToDoItemIds.Add(savedItem.Id);
+                }
+
+                return await base.HandleAsync(command, ct);
+            }
+        }
+    }
+}

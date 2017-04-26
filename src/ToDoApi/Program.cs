5104d0752c4bd882e5981787fe7521e9d16d367b
ian <ian@huddle.com>
2017-04-26 21:25:02 +0100

    Get it running in IIS to debug

--- src/ToDoApi/Program.cs
+++ src/ToDoApi/Program.cs
@@ -9,8 +9,11 @@ namespace ToDoApi
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseUrls("http://*:5000")  // listen on port 5000 on all network interfaces; needed for containers
+                .UseIISIntegration()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .CaptureStartupErrors(true)
+                .UseSetting("detailedErrors", "true")
                 .UseStartup<Startup>()
                 .Build();
 
